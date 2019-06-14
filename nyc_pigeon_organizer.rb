63c4b754fe5e0cca110244f1783f415d97e52c77
Audrea Cook <audreacwork@gmail.com>
2019-06-14 18:58:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # create empty hash
+  organized_pigeons = {}
+  
+  # search data for traits
+  data.each do |traits, traits_hash|
+    # search traits_hash for details
+    traits_hash.each do |details, details_array|
+      # search details_array for names
+      details_array.each do |name|
+        
+        # if organized_pigeons[name] doesn't exist, create empty hash
+        if organized_pigeons[name] == nil
+          organized_pigeons[name] = {}
+        end
+        
+        # if organized_pigeons[name][traits] doesn't exist, create empty array
+        if organized_pigeons[name][traits] == nil
+          organized_pigeons[name][traits] = []
+        end
+        
+        # shovel details as strings into traits array
+        organized_pigeons[name][traits] << details.to_s
+        
+      end
+    end
+  end
+  
+  organized_pigeons
 end
